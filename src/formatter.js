17aef7316cb914ed699f769b03aae42db7f8c3ee
TAWEERAT CHAIMAN <taweesoft@gmail.com>
2017-10-14 18:47:08 +0700

    except all incompetible format but datestring

--- src/formatter.js
+++ src/formatter.js
@@ -6,7 +6,7 @@ const headers = ['Date', 'Time', 'T.ID', 'GP.Vol', 'NP.Vol', 'Wat.Vol', 'GT.Vol'
 export default (pathOrText, { isPath, useHeader }) => {
     let data = [];
     let text = '';
-    if(isPath)
+    if (isPath)
         text = read(pathOrText);
     else
         text = pathOrText;
@@ -14,8 +14,17 @@ export default (pathOrText, { isPath, useHeader }) => {
         csv()
             .fromString(text)
             .on('csv', (csvRow) => {
-                if (csvRow.length > 5)
-                    data.push(csvRow);
+                if (csvRow.length !== 0) {
+                    if (csvRow.length < 5) {
+                        let timeStr = csvRow[0].substring(1, csvRow[0].length - 1);
+                        if(Date.parse(timeStr)) {
+                            data.push(csvRow);
+                        }
+                    }else {
+                        data.push(csvRow);
+                    }
+                }
+
             })
             .on('done', () => {
                 let numberOfTank = getNumberOfTank(data);
